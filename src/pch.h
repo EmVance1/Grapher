94913b571245851f4504f87e707a0dd345017cb1
Em Vance <ayv200@gmail.com>
2024-12-09 16:56:28 +0100

    Added precompiled header

---
+++ src/pch.h
@@ -0,0 +1,18 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
+#include <Windows.h>
+
+#include <cstdint>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <filesystem>
+
+#include "uuid.h"
+
